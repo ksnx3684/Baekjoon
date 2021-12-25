3a70292ff889051413300706e80b1239177305e9
ksnx3684 <84880772+ksnx3684@users.noreply.github.com>
2021-12-25 22:11:59 +0900

    Level3 - for문

---
+++ level3/No10950.java
@@ -0,0 +1,15 @@
+package level3;
+import java.util.Scanner;
+public class No10950 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int A, B;
+		for(int i = 0; i < T; i++) {
+			A = sc.nextInt();
+			B = sc.nextInt();
+			System.out.println(A+B);
+		}
+		sc.close();
+	}
+}
